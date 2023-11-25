35781ac69c58898c751e356cb5b7cb55a8779f0e
SujataTummala109 <71897878+SujataTummala109@users.noreply.github.com>
2023-11-26 05:10:35 +0530

    Update Tour.js

--- js/Tour.js
+++ js/Tour.js
@@ -22,30 +22,7 @@ AFRAME.registerComponent("tour", {
       el.setAttribute("visible", false);
     });
   },
-  createPlace: function() {
-    const details = {
-      garden: {
-        position: { x: 20, y: -4.5, z: -5.5 },
-        rotation: { x: 0, y: -90, z: 0 },
-        src: "./assets/thumbnails/garden.png",
-        title: "Garden",
-        id: "garden"
-      },
-      main_gate: {
-        position: { x: 4.6, y: -5.5, z: 25 },
-        rotation: { x: 180, y: 0, z: 0 },
-        src: "./assets/thumbnails/main_gate.png",
-        title: "Main Gate",
-        id: "main_gate"
-      },
-      home: {
-        position: { x: -9, y: 34, z: -100 },
-        rotation: { x: 0, y: 0, z: 0 },
-        src: "./assets/thumbnails/home.png",
-        title: "My Home",
-        id: "home"
-      }
-    };
+  
 
     for (var key in details) {
       const item = details[key];
